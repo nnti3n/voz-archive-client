f46d6f80722e1d1f1ae40ff11e20aec76d0d8b07
Nhat Tien <nntien@apcs.vn>
2018-01-29 13:01:12 +0700

    View thread

--- src/components/Thread.js
+++ src/components/Thread.js
@@ -1,7 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const Thread = ({ data }) => <div>Hello guys length {data.length}</div>;
+const Thread = ({ data }) => (
+  <div>
+    {data.length !== 0
+      ? data.map(item => (
+          <div>
+            <div>{item.ID}</div>
+            <div>{item.Number}</div>
+            <div>{item.UserName}</div>
+            <div>{item.Time}</div>
+            <div dangerouslySetInnerHTML={{ __html: item.Content }} />
+            <div>{item.threadID}</div>
+          </div>
+        ))
+      : ""}
+  </div>
+);
 
 const mapState = state => state.thread || [];
 
