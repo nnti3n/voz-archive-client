d7979d595e40bde0cc08c9e5594206bea503f5de
Nhat Tien <nntien@apcs.vn>
2018-02-03 17:01:12 +0700

    Add some padding thread

--- src/components/Thread.js
+++ src/components/Thread.js
@@ -1,24 +1,29 @@
 import React from "react";
 import { connect } from "react-redux";
 import styles from "../css/Thread.css";
-import { highlight } from "../css/App.css";
+import { highlight, container } from "../css/Paginate.css";
 import { visitThread } from "../actions";
 
 const Thread = ({ data, page, visitThread, currentPage, id }) => (
   <div>
-    {data.length !== 0
-      ? data.map(item => (
-          <div className={styles.post}>
-            <div>#{item.Number}</div>
-            <div>{item.ID}</div>
-            <div>{item.UserName}</div>
-            <div>{item.Time}</div>
-            <div dangerouslySetInnerHTML={{ __html: item.Content }} />
-            <div>{item.threadID}</div>
-          </div>
-        ))
-      : ""}
     <div>
+      {data.length !== 0
+        ? data.map(item => (
+            <div className={styles.post}>
+              <div>#{item.Number}</div>
+              <div>{item.ID}</div>
+              <div>{item.UserName}</div>
+              <div>{item.Time}</div>
+              <div
+                className={styles.content}
+                dangerouslySetInnerHTML={{ __html: item.Content }}
+              />
+              <div>{item.threadID}</div>
+            </div>
+          ))
+        : ""}
+    </div>
+    <div className={container}>
       {data.length > 0
         ? Array.from(Array(page).keys()).map(item => (
             <a
