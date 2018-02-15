1c965dbe4f48257f700d967c686472c4b1838080
Nhat Tien <nntien@apcs.vn>
2018-02-16 02:27:27 +0700

    New year commit fix box highlight

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,7 +4,7 @@ import { NavLink } from "redux-first-router-link";
 import { goToPage } from "../actions";
 import styles from "../css/Sidebar.css";
 
-const Sidebar = ({ onClick, path }) => (
+const Sidebar = ({ onClick, path, boxID }) => (
   <div className={styles.sidebar}>
     <div className={styles.fixedContainer}>
       <h2>SEO-FRIENDLY LINKS</h2>
@@ -24,10 +24,7 @@ const Sidebar = ({ onClick, path }) => (
         HOME
       </span>
 
-      <span
-        className={active(path, /\/box\/33\w*/)}
-        onClick={() => onClick("BOX", 33)}
-      >
+      <span className={activeBox(boxID, 33)} onClick={() => onClick("BOX", 33)}>
         F33
       </span>
     </div>
@@ -38,7 +35,14 @@ const active = (currentPath, path) => {
   return currentPath.match(path) ? styles.active : "";
 };
 
+const activeBox = (currentBox, boxID) => {
+  return currentBox === boxID ? styles.active : "";
+};
+
 const mapDispatch = { onClick: goToPage };
-const mapState = ({ location }) => ({ path: location.pathname });
+const mapState = ({ location, box }) => ({
+  path: location.pathname,
+  boxID: box.id
+});
 
 export default connect(mapState, mapDispatch)(Sidebar);
