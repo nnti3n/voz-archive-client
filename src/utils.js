d6019a71320e893e30579905dd8153e667954238
Nhat Tien <nntien@apcs.vn>
2018-03-22 02:07:55 +0700

    production endpoint

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,9 @@
 export const isServer = typeof window === "undefined";
 
+const VOZ_API =
+  process.env.NODE_ENV === "production"
+    ? "http://ec2-54-169-119-62.ap-southeast-1.compute.amazonaws.com:8080"
+    : "http://localhost:8080";
+
 export const fetchData = async path =>
-  fetch(`http://localhost:8080${path}`).then(data => data.json());
+  fetch(`${VOZ_API}/${path}`).then(data => data.json());
