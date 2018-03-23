205e410e97cd2b5690edb8b6818f29f66360ba20
Nhat Tien <nntien@apcs.vn>
2018-03-23 11:04:28 +0700

    New endpoint

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ export const isServer = typeof window === "undefined";
 
 const VOZ_API =
   process.env.NODE_ENV === "production"
-    ? "http://ec2-54-169-119-62.ap-southeast-1.compute.amazonaws.com:8080"
+    ? "http://vozarchive.me:8080"
     : "http://localhost:8080";
 
 export const fetchData = async path =>
