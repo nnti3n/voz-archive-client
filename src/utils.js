c438803379de6c4e2b8bcb23be3fe81cd3355634
Nhat Tien <nntien@apcs.vn>
2018-04-23 09:49:22 +0700

    fix dev log

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,12 @@
 export const isServer = typeof window === "undefined";
 
-console.log(process.env.API);
-console.log(process.env.NODE_ENV);
-
 const VOZ_API =
   process.env.NODE_ENV === "production"
     ? process.env.API
     : "http://localhost:8080";
+
+console.log(VOZ_API);
+console.log(process.env.NODE_ENV);
 
 export const fetchData = async path =>
   fetch(`${VOZ_API}${path}`).then(data => data.json());
