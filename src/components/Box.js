e25354734094a22f8e9fa968166650d917560a90
Nhat Tien <nntien@apcs.vn>
2018-04-25 17:02:15 +0700

    Styling

--- src/components/Box.js
+++ src/components/Box.js
@@ -4,26 +4,29 @@ import React from "react";
 import { connect } from "react-redux";
 import { goToPage, visitBoxPage } from "../actions";
 import Pagination from "./Pagination";
-import styles from "../css/Box.css";
-// import { excludeThreads } from "../utils";
+import styles from "../css/Box.scss";
+import { formatDate } from "../utils";
 
 const Box = ({ data, visitBoxPage, goToPage, page, id, currentPage }) => (
   <div>
     {data.length
       ? data.map(item => (
-          <div
-            className={styles.card}
-            key={item.ID}
-            onClick={() => goToPage("THREAD", item.ID)}
-          >
-            <div>{item.ID}</div>
-            <div>{item.Title}</div>
-            {/*<div>{item.Source}</div>*/}
-            <div>{item.PageCount}</div>
-            <div>{item.PostCount}</div>
-            <div>{item.ViewCount}</div>
-            <div>{item.UserNameStarter}</div>
-            <div> </div>
+          <div className={styles.story} key={item.ID}>
+            <h3 onClick={() => goToPage("THREAD", item.ID)}>{item.Title}</h3>
+            <div>
+              <span>{item.UserNameStarter}</span>
+              <span>{formatDate(item.LastUpdated)}</span>
+            </div>
+            <div>
+              <span>Comment {item.PostCount}</span>
+              <span>View {item.ViewCount}</span>
+            </div>
+            <div>
+              <a
+                href={`https://vozforums.com/showthread.php?t=${item.ID}`}
+                target="_blank"
+              >{`Thread ${item.ID}`}</a>
+            </div>
           </div>
         ))
       : "NotFound"}
