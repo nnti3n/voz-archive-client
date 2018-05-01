1ed0200c76b9f6f645f2cebbe9163dbb168c2c79
Nhat Tien <nntien@apcs.vn>
2018-05-01 16:56:05 +0700

    Update loading animation

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,6 +3,10 @@ import { spinner } from "../css/Switcher.css";
 
 export default () => (
   <div className={spinner}>
-    <div />
+    <div>
+      <div className="bounce1" />
+      <div className="bounce2" />
+      <div className="bounce3" />
+    </div>
   </div>
 );
