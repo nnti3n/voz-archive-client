7197ea90b6c5a31399157041a0a06a5baad0c0a9
Nhat Tien <nntien@apcs.vn>
2018-05-13 23:53:19 +0700

    Auto hide sidebar on mobile

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,23 +2,54 @@ import React from "react";
 import { connect } from "react-redux";
 // import { NavLink } from "redux-first-router-link";
 import { goToPage } from "../actions";
-import styles from "../css/Sidebar.css";
+import styles from "../css/Sidebar.scss";
 
 // const Sidebar = ({ onClick, path, boxID }) => (
-const Sidebar = ({ onClick, boxID }) => (
-  <div className={styles.sidebar}>
-    <div className={styles.fixedContainer}>
-      <h2>Boxes</h2>
+class Sidebar extends React.Component {
+  state = { isHide: false };
+  componentDidMount() {
+    window.addEventListener("scroll", this.hideBar);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.hideBar);
+  }
+  hideBar = () => {
+    let { isHide } = this.state;
+    window.scrollY > this.prev
+      ? !isHide && this.setState({ isHide: true })
+      : isHide && this.setState({ isHide: false });
 
-      <span className={activeBox(boxID, 33)} onClick={() => onClick("BOX", 33)}>
-        F33
-      </span>
-      <span className={activeBox(boxID, 17)} onClick={() => onClick("BOX", 17)}>
-        F17
-      </span>
-    </div>
-  </div>
-);
+    this.prev = window.scrollY;
+  };
+
+  render() {
+    const { onClick, boxID } = this.props;
+    const { isHide } = this.state;
+    return (
+      <div className={styles.sidebar}>
+        <div
+          className={styles.fixedContainer}
+          style={{ transform: isHide ? "translateY(-100px)" : "translateY(0)" }}
+        >
+          <h2>Boxes</h2>
+
+          <span
+            className={activeBox(boxID, 33)}
+            onClick={() => onClick("BOX", 33)}
+          >
+            F33
+          </span>
+          <span
+            className={activeBox(boxID, 17)}
+            onClick={() => onClick("BOX", 17)}
+          >
+            F17
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
 
 // const active = (currentPath, path) => {
 //   return currentPath.match(path) ? styles.active : "";
