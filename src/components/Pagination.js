f77529fea39509d353a352daee357573054aca07
Nhat Tien <nntien@apcs.vn>
2018-05-25 15:53:17 +0700

    update pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,32 +1,48 @@
 import React from "react";
-import { container } from "../css/Paginate.css";
+import styles from "../css/Paginate.scss";
 
 const Pagination = ({ page, currentPage, visitPage, id }) => (
-  <div className={container}>
+  <div className={styles.container}>
     <a
-      style={{ visibility: currentPage === 1 ? "hidden" : "visible" }}
+      style={{ display: currentPage < 3 ? "none" : "inline" }}
+      onClick={() => {
+        visitPage(id, 1);
+      }}
+    >
+      First
+    </a>
+    <a
+      style={{ display: currentPage === 1 ? "none" : "inline" }}
       onClick={() => {
         if (currentPage > 1 && currentPage <= page) {
           visitPage(id, currentPage - 1);
         }
       }}
     >
-      Prev{" "}
+      Prev
     </a>
     <a
-      style={{ visibility: currentPage === page ? "hidden" : "visible" }}
+      style={{ display: currentPage === page ? "none" : "inline" }}
       onClick={() => {
         if (currentPage < page) {
           visitPage(id, currentPage + 1);
         }
       }}
     >
-      Next{" "}
+      Next
     </a>
     page:{" "}
     <span>
-      {currentPage} of {page}
+      {currentPage} of {page}{" "}
     </span>
+    <a
+      style={{ display: currentPage > page - 2 ? "none" : "inline" }}
+      onClick={() => {
+        visitPage(id, page);
+      }}
+    >
+      Last
+    </a>
   </div>
 );
 
