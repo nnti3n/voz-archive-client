58a402c3788cf02f1f1fe1fa6efc1386154b234f
Nhat Tien <nntien@apcs.vn>
2018-05-25 18:08:03 +0700

    add credit

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,21 @@ import React from "react";
 import Sidebar from "./Sidebar";
 import Switcher from "./Switcher";
 
-import styles from "../css/App.css";
+import styles from "../css/App.scss";
 
 export default () => (
-  <div>
+  <div className={styles.container}>
     <div className={styles.app}>
       <Sidebar />
       <Switcher />
     </div>
+    <footer className={styles.footer}>
+      <div>
+        Hello this site is made by{" "}
+        <a href="https://github.com/nnti3n" target="_blank">
+          nnti3n
+        </a>
+      </div>
+    </footer>
   </div>
 );
