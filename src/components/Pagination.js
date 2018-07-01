ce0b11669cfcde001554d056330d1bbd0269e982
Nhat Tien <nntien@apcs.vn>
2018-07-01 22:39:56 +0700

    Paginate by left and right arrow key

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,49 +1,92 @@
 import React from "react";
+import T from "prop-types";
 import styles from "../css/Paginate.scss";
 
-const Pagination = ({ page, currentPage, visitPage, id }) => (
-  <div className={styles.container}>
-    <a
-      style={{ display: currentPage < 3 ? "none" : "inline" }}
-      onClick={() => {
-        visitPage(id, 1);
-      }}
-    >
-      First
-    </a>
-    <a
-      style={{ display: currentPage === 1 ? "none" : "inline" }}
-      onClick={() => {
-        if (currentPage > 1 && currentPage <= page) {
-          visitPage(id, currentPage - 1);
-        }
-      }}
-    >
-      Prev
-    </a>
-    <a
-      style={{ display: currentPage === page ? "none" : "inline" }}
-      onClick={() => {
-        if (currentPage < page) {
-          visitPage(id, currentPage + 1);
-        }
-      }}
-    >
-      Next
-    </a>
-    page:{" "}
-    <span>
-      {currentPage} of {page}{" "}
-    </span>
-    <a
-      style={{ display: currentPage > page - 2 ? "none" : "inline" }}
-      onClick={() => {
-        visitPage(id, page);
-      }}
-    >
-      Last
-    </a>
-  </div>
-);
+let RIGHT_ARROW = 39;
+let LEFT_ARROW = 37;
+
+class Pagination extends React.Component {
+  componentDidMount() {
+    window.addEventListener("keydown", this._handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this._handleKeyDown);
+  }
+
+  _handleKeyDown = () => {
+    switch (event.keyCode) {
+      case RIGHT_ARROW:
+        this.nextPage();
+        break;
+      case LEFT_ARROW:
+        this.prevPage();
+        break;
+      default:
+        break;
+    }
+  };
+
+  prevPage = () => {
+    const { page, currentPage, visitPage, id } = this.props;
+    if (currentPage > 1 && currentPage <= page) {
+      visitPage(id, currentPage - 1);
+    }
+  };
+
+  nextPage = () => {
+    const { page, currentPage, visitPage, id } = this.props;
+    if (currentPage < page) {
+      visitPage(id, currentPage + 1);
+    }
+  };
+
+  render() {
+    const { page, currentPage, visitPage, id } = this.props;
+    return (
+      <div className={styles.container}>
+        <a
+          style={{ display: currentPage < 3 ? "none" : "inline" }}
+          onClick={() => {
+            visitPage(id, 1);
+          }}
+        >
+          First
+        </a>
+        <a
+          style={{ display: currentPage === 1 ? "none" : "inline" }}
+          onClick={this.prevPage}
+        >
+          Prev
+        </a>
+        <a
+          style={{ display: currentPage === page ? "none" : "inline" }}
+          onClick={this.nextPage}
+        >
+          Next
+        </a>
+        page:{" "}
+        <span>
+          {currentPage} of {page}{" "}
+        </span>
+        <a
+          style={{ display: currentPage > page - 2 ? "none" : "inline" }}
+          onClick={() => {
+            visitPage(id, page);
+          }}
+        >
+          Last
+        </a>
+      </div>
+    );
+  }
+}
+
+Pagination.propTypes = {
+  id: T.oneOfType([T.string, T.number]),
+  page: T.number,
+  currentPage: T.number,
+  visitPage: T.func
+};
 
 export default Pagination;
